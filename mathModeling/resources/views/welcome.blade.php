630689c92727206f302e81bf7c784c9271f8c5f0
Ahmed-pc <perfect_ahmed@yahoo.com>
2019-04-22 01:23:07 +0300

    about us section

--- mathModeling/resources/views/welcome.blade.php
+++ mathModeling/resources/views/welcome.blade.php
@@ -105,16 +105,16 @@
         <div class="row about_content align-items-center">
             <div class="col-lg-6">
                 <div class="section_content">
-                    <h6>About Us</h6>
-                    <h1>We Believe that <br>Interior beauty Lasts Long</h1>
-                    <p>inappropriate behavior is often laughed off as “boys will be boys,” women face higher conduct standards
-                        especially in the workplace. That’s why it’s crucial that as women.</p>
-                    <a class="primary_btn" href="#">Learn More</a>
+                    <h6>О нас</h6>
+                    <h1>Мат. Mоделирование</h1>
+                    <p>Мы специализируемся по расчетам на прочность - статическую, динамическую. Решаем задачи газодинамики и термодинамики!
+                        Владеем основами расчетов на устойчивость и классом нелинейных задач (контактные и пластические НДС). </p>
+                    <a class="primary_btn" href="#">Узнай больше</a>
                 </div>
             </div>
             <div class="col-lg-6">
                 <div class="about_us_image_box justify-content-center">
-                    <img class="img-fluid w-100" src="/mathModeling/public/img/about_img.png" alt="">
+                    <img class="img-fluid w-100" src="/mathModeling/public/img/about_img (1).png" alt="">
                 </div>
             </div>
         </div>
